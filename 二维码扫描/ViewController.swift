e017ffa16a4f46a214f9f41edf932c42ad39bcf4
任玉飞 <872943400@qq.com>
2016-04-29 15:38:27 +0800

    初始化提交

---
+++ 二维码扫描/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  二维码扫描
+//
+//  Created by 任玉飞 on 16/4/29.
+//  Copyright © 2016年 任玉飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
